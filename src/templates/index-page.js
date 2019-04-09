5baa7b66c6e896cb6f60dc2031be6e739e0e1ae2
LimeWub <myrto.kontouli@gmail.com>
2019-04-09 11:18:12 +0100

    [Styles] Super basic styles setup

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -156,7 +156,11 @@ export default IndexPage
 
 export const pageQuery = graphql`
   query IndexPageTemplate {
-    markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
+    markdownRemark(
+      frontmatter: { 
+        templateKey: { eq: "index-page" } 
+      }
+    ) {
       frontmatter {
         title
         image {
